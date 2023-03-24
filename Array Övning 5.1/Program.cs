a36d25f28ef1197ef7d4e399f19aedcddf31a945
Anton <>
2023-03-24 09:08:52 +0100

    Add project files.

---
+++ Array Övning 5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] i = new string[3];
+
+            i[0] = "Borås";
+            Console.WriteLine("Stad 1 = " + i[0]);
+
+            i[1] = "Växjö";
+            Console.WriteLine("Stad 2 = " + i[1]);
+
+            Console.Write("Skriv namnet på tredje stad : ");
+            i[2] = Console.ReadLine();
+            Console.WriteLine(i[0] + " " + i[1] + " " + i[2]);
+
+        }
+    }
+}
